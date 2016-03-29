d850d0f1560025bd3fd9f0f5e4d2bc5317ecc53a
WX-T <154199219@qq.com>
2016-03-29 11:18:58 +0800

    Update resoult.py

--- resoult.py
+++ resoult.py
@@ -1,2 +1,4 @@
 #Python 字符串的格式化
 https://mp.weixin.qq.com/s?__biz=MzI0NjIxMzE5OQ==&mid=408127174&idx=1&sn=e8e355fa181d995eeb6110e33308df96&scene=0&previewkey=4ZetsO9MX0lk25jVyFuet8wqSljwj2bfCUaCyDofEow%3D&key=710a5d99946419d9b9bfc50d41dc67766e2e268017646fbdfbd26bc5d9e71d63f8fd57f50f8dd3e882e010c51da03fc7&ascene=0&uin=NzI5MTc0NjYw&devicetype=iMac+MacBookPro12%2C1+OSX+OSX+10.11.1+build%2815B42%29&version=11020201&pass_ticket=JW7IHMFzmc3Y%2FDLMoh8sVVfF3lW7ZA0KZTuL5V9uudk9VFdF5Ibo9%2BBbnAtsMB2L
+#python 库
+https://www.zhihu.com/question/24590883
