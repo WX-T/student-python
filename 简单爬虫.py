e8c7101fa2f40a2c19554f9afe8cc069c93c389a
WX-T <154199219@qq.com>
2016-03-29 10:49:48 +0800

    Update 简单爬虫.py

--- 简单爬虫.py
+++ 简单爬虫.py
@@ -1,1 +1,11 @@
+#encoding:UTF-8
+#简单爬虫
+import urllib.request
+import re
 
+url = "http://wenku.it168.com/list/database_oracle_0_0_0_0_0_0_0_1.shtml"
+data = urllib.request.urlopen(url).read()
+data = data.decode('UTF-8')
+r = r'<a .*?>(.*?)[\s\S]</a>'
+lists = re.findall(r,data)
+print(lists)
